5f78244c7e3b416929efbd57e63aa753358621f6
Ivsasa <gagochorenmartin@gmail.com>
2022-05-13 14:43:41 +0200

    primeros pasos de la plantilla
    generar una plantilla de curriculum con vue 2 + bootstrap, bootstrapVue

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/grid',
+    name: 'grid',
+   
+    component: () => import(/* webpackChunkName: "about" */ '../views/Grid.vue')
+  },
+  {
+    path: '/cards',
+    name: 'cards',
+   
+    component: () => import(/* webpackChunkName: "about" */ '../views/Cards.vue')
+  },
+  {
+    path: '/form',
+    name: 'form',
+   
+    component: () => import(/* webpackChunkName: "about" */ '../views/Form.vue')
   }
 ]
 
---
+++ src/views/Cards.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mt-5">
+      <b-button variant="success">
+          Texto
+      </b-button>
+      <b-button variant="info">
+          Texto
+      </b-button>
+      <b-button variant="danger">
+          Texto
+      </b-button>
+      <b-button variant="primary">
+          Texto
+      </b-button>
+      <b-button variant="warning">
+          Texto
+      </b-button>
+      
+
+
+    <b-row class="mt-5">
+        <b-col cols="12" md="4">
+            <b-card
+            title="Card Title"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+            >
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda maxime distinctio, reiciendis est minus optio praesentium, impedit maiores atque consectetur possimus laboriosam nam magnam a ipsa alias delectus. Debitis, voluptatem?</p>
+                <b-button variant="dark" href="#">
+          Texto
+      </b-button>
+            </b-card>
+        </b-col>
+    </b-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Cards'
+}
+</script>
